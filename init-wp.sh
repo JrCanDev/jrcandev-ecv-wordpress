0ded1e52babcc4a71d81db38cf2e73d85faae06f
Neekzen <neekzen.62@gmail.com>
2024-05-07 09:57:27 +0200

    bdd connexion issue

--- init-wp.sh
+++ init-wp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-# Exporter la variable SITE_URL
+# Variables d'environnements
 export SITE_URL=${SITE_URL:-defaulturl.com}
 export ADMIN_USER=${ADMIN_USER:-defaultuser}
 export ADMIN_PASSWORD=${ADMIN_PASSWORD:-defaultpassword}
@@ -10,7 +10,6 @@ export WP_TITLE=${WP_TITLE:-defaulttitle}
 export WP_IS_MULTISITE_SUBDOMAIN=${WP_MULTISITE_DOMAINE:-false}
 
 
-# Utiliser la variable SITE_URL
 echo "URL is $SITE_URL"
 
 # Appeler le script d'entrée original de WordPress pour initialiser correctement l'environnement
@@ -30,7 +29,7 @@ done
 # Installer WordPress si non installé
 if ! wp core is-installed --allow-root; then
     wp core install --url=$SITE_URL --title=$WP_TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --allow-root
-    wp core multisite-install --allow-root --url=$SITE_URL --base="/" --subdomains=false --title=$WP_TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL
+    wp core multisite-install --allow-root --url=$SITE_URL --base="/" --subdomains=$WP_IS_MULTISITE_SUBDOMAIN --title=$WP_TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL
 fi
 
 # Attendre indéfiniment pour garder le conteneur en cours d'exécution en arrière-plan
